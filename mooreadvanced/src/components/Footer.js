62649ea455725e579f7c98db53703c0d506bfbe2
Patrick <kimanthip@gmail.com>
2023-06-22 15:14:07 +0300

    Design refinement 2

--- mooreadvanced/src/components/Footer.js
+++ mooreadvanced/src/components/Footer.js
@@ -80,7 +80,6 @@ function Footer() {
       </div>
     </footer>
   );
-
 }
 
 export default Footer;
--- mooreadvanced/src/components/LowerHeader.js
+++ mooreadvanced/src/components/LowerHeader.js
@@ -1,14 +1,15 @@
-const LowerHeader = ({ textOne = null, textTwo }) => {
+const LowerHeader = ({ text }) => {
   return (
-    <header className='header container mb-section'>
-      <img
+    <header className='lower-header container mb-section'>
+      <p>{text}</p>
+      {/* <img
         src='../assets/logo/Moore Advanced Solutions (Gold).svg'
         alt='watermark'
       />
       <div class='header-text'>
         {textOne && <p>{textOne}</p>}
         <p>{textTwo}</p>
-      </div>
+      </div> */}
     </header>
   );
 };
